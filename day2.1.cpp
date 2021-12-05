ed95ab83aabe34aef72108753ef839a827213306
Leon Schenk <989485+leonschenk@users.noreply.github.com>
2021-12-06 00:35:52 +0100

    Create day2.1.cpp

---
+++ day2.1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+int main(void){
+    std::ifstream infile("File2");
+    
+    int a;
+    std::string dir;
+    int hor, depth = 0;
+    while (infile >> dir >> a) {
+        switch (dir[0]) {
+            case 'u':
+                depth -= a;
+                break;
+            case 'd':
+                depth += a;
+                break;
+            case 'f':
+                hor += a;
+                break;
+            default:
+                std::cout << "unkown direction: " << dir << std::endl;
+        }
+    }
+    std::cout << "hor: " << hor << " depth: " << depth << " total: " << (hor * depth) << std::endl;
+}
