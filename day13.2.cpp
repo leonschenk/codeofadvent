487d98b90016897ab0e164cad373d6a376dd8246
Leon Schenk <989485+leonschenk@users.noreply.github.com>
2021-12-13 08:58:12 +0100

    Create day13.2.cpp

---
+++ day13.2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+
+int main(void){
+    std::ifstream infile("File18");
+    
+    std::string line;
+    int x, y;
+    char b, d;
+    int fold;
+    
+    std::set<std::pair<int, int>> paper;
+    
+    while (infile >> x >> b >> y) {
+        paper.insert({x, y});
+    }
+    infile.clear();
+    while (infile >> line >> line >> b >> d >> fold) {
+        std::vector<std::pair<int, int>> toFold;
+        for (auto dot = paper.begin(); dot != paper.end();) {
+            bool sw = b == 'x';
+            int val = sw ? dot->first : dot->second;
+            int otherval = sw ? dot->second : dot->first;
+            int newVal = (fold<<1) - val;
+            if (val > fold) {
+                toFold.push_back({sw ? newVal : otherval, sw ? otherval : newVal});
+                dot = paper.erase(dot);
+            } else {
+                dot++;
+            }
+        }
+        for (auto dot : toFold) {
+            paper.insert(dot);
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 100; j++) {
+            if (paper.contains({j, i})) {
+                std::cout << '*';
+            } else {
+                std::cout << ' ';
+            }
+        }
+        std::cout << std::endl;
+    }
+}
