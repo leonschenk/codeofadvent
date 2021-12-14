a461bbaaf64ee3eea79f2c27dfe7ffed1c5e4c86
Leon Schenk <989485+leonschenk@users.noreply.github.com>
2021-12-15 00:48:52 +0100

    Update day14.2.cpp

--- day14.2.cpp
+++ day14.2.cpp
@@ -22,6 +22,7 @@ int main(void){
     std::string startposition;
     infile >> startposition;
 
+    // inlezen van de regels en vertalen naar indices
     int curInd = 0;
     char first, second, a, right;
     while (infile >> first >> second >> a >> a >> right) {
@@ -42,11 +43,13 @@ int main(void){
         rules[cToInd[first]][cToInd[second]] = cToInd[right];
     }
     
+    // per step alle opties doorrekenen (hoeft slechts 1 stap terug te kijken)
     for (int i = 0; i < steps; i++) {
         int stepIt = i&0x01;
         int prevStepIt = stepIt^0x01;
         for (int j = 0; j < options; j++) {
             for (int k = 0; k < options; k++) {
+                // tel bij de getallen het geinserteerde getal plus van de vorige step de geïnserteerde getallen links en rechts
                 int insertedAtThisLevel = rules[j][k];
                 for (int l = 0; l < options; l++) {
                     cache[stepIt][j][k][l] = cache[prevStepIt][j][insertedAtThisLevel][l] + cache[prevStepIt][insertedAtThisLevel][k][l];
@@ -63,6 +66,7 @@ int main(void){
     for (int i = 0; i < startposition.size() - 1; i++) {
         int ind = cToInd[startposition[i]];
         int ind2 = cToInd[startposition[i+1]];
+        // tel alle getallen op van alle paartjes in de startposition
         for (int j = 0; j < options; j++) {
             res[j] += cache[(steps + 1) & 0x01][ind][ind2][j];
         }
